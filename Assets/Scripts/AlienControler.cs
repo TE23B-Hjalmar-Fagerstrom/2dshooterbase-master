f6950aad6f285b8590fd470030fb8d296fec7411
Hjalmar Fagerström <hjalmar.fagerstrom@elev.ga.ntig.se>
2025-11-05 09:48:54 +0100

    sixth commit

--- Assets/Scripts/AlienControler.cs
+++ Assets/Scripts/AlienControler.cs
@@ -8,11 +8,27 @@ public class AlienControler : MonoBehaviour
     float timeSincelastTurn = 0;
     [SerializeField]
     float timeBetweenTurn = 1.5f;
-        
+
+    public Animator animator;
+
+    [SerializeField]
+    GameObject groundCheckerLeft;
+    [SerializeField]
+    GameObject groundCheckerRight;
+
+    [SerializeField]
+    LayerMask groundLayer;
+
     Vector2 movement = Vector2.left;
 
     void Update()
     {
+        bool isGroundedleft = Physics2D.OverlapCircle(
+        groundCheckerLeft.transform.position,
+        .1f,
+        groundLayer
+        );
+
         timeSincelastTurn += Time.deltaTime;
 
         if (timeSincelastTurn >= timeBetweenTurn)
@@ -22,5 +38,15 @@ public class AlienControler : MonoBehaviour
         }
 
         transform.Translate(movement * speed * Time.deltaTime);
+
+        if (movement.x > 0)
+        {
+            animator.SetBool("walking left", false);
+        }
+        else
+        {
+            animator.SetBool("walking left", true);
+        }
+
     }
 }
